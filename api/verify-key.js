eb3aaf9953eca65b00a1ae9d9875c9c3cdb8a766
Tlongdev-vn <thienlong4712@gmail.com>
2026-09-07 17:27:38 +0700

    Add CORS headers to verify-key handler

--- api/verify-key.js
+++ api/verify-key.js
@@ -2,6 +2,12 @@ import { validateKey } from './database.js';
 
 export default function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, User-Agent');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
     
     const { hwid, key } = req.query;
 
